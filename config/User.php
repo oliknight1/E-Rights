937271dc1e0fe8a6a3c654881f9fd32bf3ff09b6
oliknight1 <oliknight2014@gmail.com>
2020-04-14 14:44:50 +0100

    Changed img and testing on load, not working currently

---
+++ config/User.php
@@ -0,0 +1,68 @@
+<?php
+
+class User
+{
+    // Database variables
+    private $conn;
+    private $table = 'user';
+
+    // User variables
+
+    private $id;
+    private $username;
+    private $password;
+    private $security_question;
+    private $security_answer;
+
+    public function __construct($db)
+    {
+        $this->conn = $db->connect();
+    }
+
+    // Grabs every users inside the table
+    public function getUsers()
+    {
+        $query = "SELECT * from `users` ORDER BY `id` ASC";
+
+        $result = $this->conn->query($query);
+
+        return $result;
+    }
+
+
+
+
+
+    public function getSingleUserById($data)
+    {
+
+        // if $data is a int, then it will be an id
+        // if $data is a string, it will be a username
+        if (is_int($data)) {
+
+            $query = "SELECT * from `users` WHERE `id` = " . $data . " LIMIT 0,1";
+        } else if (is_string($data)) {
+
+
+            $query = "SELECT * from `users` WHERE `username` = \"" . $data . "\" LIMIT 0,1";
+        }
+
+        $result = $this->conn->query($query);
+        // Check there are
+        if ($result->num_rows > 0) {
+
+            $row = $result->fetch_assoc();
+
+            // Set user properties 
+            $this->id = $row["id"];
+            $this->username = $row["username"];
+            $this->password = $row["password"];
+            $this->security_question = $row["security_question"];
+            $this->security_answer = $row["security_answer"];
+
+            return json_encode($row);
+        } else {
+            echo "No users found";
+        }
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const loginImg = document.querySelector(".login-img-container img");
+loginImg.addEventListener("load", () => {
+    // Use opacity as somr browsers will not attempt to load an image if display:none
+    loginImg.style.opacity = "1";
+    document.querySelector(".login-form-container").style.animation = "loginAnimation forwards 1.5s";
+
+})
