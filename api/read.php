6d43e9c6f361387a3837eeb3478beab0debdf323
oliknight1 <oliknight2014@gmail.com>
2020-04-23 17:06:06 +0100

    Can now create a new user account, need to secure inputs

---
+++ api/read.php
@@ -0,0 +1,47 @@
+<?php
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../config/Database.php';
+include_once '../config/User.php';
+
+// Instantiate database and connect to it
+$db = new Database();
+$db->connect();
+
+// Instantiate user object
+$user = new User($db);
+
+$result = $user->getUsers();
+$rowCount = $result->num_rows;
+// Check if there are any users
+
+if ($rowCount > 0) {
+    $userArr = array();
+    $userArr['data'] = array();
+
+    while ($row = $result->fetch_assoc()) {
+        extract($row);
+        // array keys needs to be the same as column names in db
+        $userItem = array(
+            'id' => $id,
+            'username' => $username,
+            'password' => $password,
+            'security_question' => $security_question,
+            'security_answer' => $security_answer
+        );
+        // Push to 'data'
+        array_push($userArr['data'], $userItem);
+    }
+    // Turn into JSON & output
+
+    echo json_encode($userArr);
+} else {
+    // No users
+
+    echo json_encode(
+        array('message' => "No Users Found")
+    );
+}
--- sign-up.php
+++ sign-up.php
@@ -1,3 +1,129 @@
+<?php
+
+require_once "config/Database.php";
+
+$username = '';
+$usernameErr = '';
+
+$password = '';
+$passwordErr = '';
+
+$repeatPass = '';
+$repeatPassErr = '';
+
+$securityAnswer = '';
+$securityAnswerErr = '';
+
+$db = new Database();
+$conn = $db->connect();
+// Check if POST request was made
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Valdiate username 
+    if (empty(trim($_POST['username']))) {
+        $usernameErr = 'Please enter a username';
+    } else {
+
+        // Create a prepared statement to protect against SQL injection
+        // We later bind ? to the username
+        $sql = "SELECT id FROM users WHERE username = ?";
+        // Check the statement prepares successfully
+        if ($stmt = $conn->prepare($sql)) {
+
+            // // This binds the username to the ? in the sql statement
+            // // 's' stands for string
+            $stmt->bind_param('s', $paramUsername);
+
+            // // Remove whitespace before and after username
+            $paramUsername = trim($_POST['username']);
+
+            // // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                // Store result
+                $stmt->store_result();
+
+                if ($stmt->num_rows == 1) {
+                    $usernameErr = "Username has already been taken";
+                } else {
+                    $username = strip_tags(trim($_POST['username']));
+                }
+            } else {
+            }
+
+            $stmt->close();
+        }
+    }
+    // // Validate password 
+    if (empty(trim($_POST['password']))) {
+        $passwordErr = 'Please enter a password';
+    } elseif (strlen(trim($_POST['password'])) < 5) {
+
+        $passwordErr = "Password needs to be at least 5 characters";
+    } else {
+        $password = strip_tags(trim($_POST['password']));
+
+        // Validate the repeated password
+        if (empty(trim($_POST['repeat-password']))) {
+            $repeatPassErr = 'Please repeat your password';
+        } else {
+            $repeatPass = trim($_POST['repeatPassword']);
+            if (empty($repeatPassErr) && ($password != $repeatPass)) {
+                $repeatPassErr = 'Passwords do not match, please try again';
+            }
+        }
+
+        // // Validate secuirty answer
+        if (empty(trim($_POST['security-answer']))) {
+            $securityAnswerErr = "Please enter a security answer";
+            echo "<h1>ERROR</h1>";
+        } else {
+            $securityAnswer = strip_tags(trim($_POST['securityAnswer']));
+        }
+
+        // Check for any input errors before inserting data into the database
+        if (empty($usernameErr) && empty($passwordErr) && empty($securityAnswerErr)) {
+
+
+            // Prepare the insert statement
+            $sql = "INSERT INTO `users`( `username`, `password`, `security_question`, `security_answer`) VALUES (?,?,?,?)";
+
+            if ($stmt = $conn->prepare($sql)) {
+
+
+                // Bind variables to the prepared statment 
+                $stmt->bind_param("ssss", $paramUsername, $paramPass, $paramSecurityQ, $paramSecurityA);
+
+                // Set the parameters
+                $paramUsername = $username;
+
+                // Hash the password so the users actual password isn't saved in a database, instead a hashed version is stored
+                $paramPass = password_hash($password, PASSWORD_DEFAULT);
+
+                if (isset($_POST['security-question'])) {
+                    $paramSecurityQ = $_POST['security-question'];
+                }
+
+                if (isset($_POST['security-answer'])) {
+                    $paramSecurityA = $_POST['security-answer'];
+                }
+
+                // Execute prepared statement
+                if ($stmt->execute()) {
+
+                    //
+                } else {
+                    // echo "<h1>errror</h1>";
+                }
+                $stmt->close();
+            }
+        }
+        $conn->close();
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,7 +141,7 @@
     <div class="user-form-wrapper">
 
         <div class="login-img-container">
-            <img src="./assets/login-img.jpeg" alt="">
+            <img src="./assets/login-img.jpeg" class="signup-img">
         </div>
         <div class="user-form-container signup">
             <div class="signup-title">
@@ -23,26 +149,26 @@
             </div>
             <img src="assets/illustrations/sign-up-img.svg" alt="">
             <form method="POST">
-                <input type="text" placeholder="Username" required>
+                <input type="text" placeholder="Username" required name="username">
 
-                <input type="password" placeholder="Password" required>
+                <input type="password" placeholder="Password" required name="password">
 
-                <input type="password" placeholder="Repeat Password" required>
+                <input type="password" placeholder="Repeat Password" required name="repeat-password">
 
 
-                <select name="security-question" id="" required>
+                <select name="security-question" required>
                     <!-- First option is the default
                          disabled means it cannot be selected by the user
                          hidden means it cannot be seen by the use
                     -->
-                    <option value="" selected="selected" disabled hidden> Security Question</option>
-                    <option value="0">Test quesion 1</option>
-                    <option value="1">Test quesion 1</option>
+                    <option selected="selected" disabled hidden>Security Question</option>
+                    <option>Test quesion 1</option>
+                    <option>Test quesion 2</option>
                 </select>
 
 
 
-                <input type="text" placeholder="Answer" required>
+                <input type="text" placeholder="Answer" required name="security-answer">
                 <div class="form-links">
                     <p>Already signed up?</p>
                     <a href="login.php" class="sign-in-link">Sign in</a>
