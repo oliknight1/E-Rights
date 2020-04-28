71973ea232b2043cdb8388d7063596699424b096
SilasParker <silas.parker@gmail.com>
2020-04-28 13:09:28 +0100

    Slider working
    Slider now working, needs styling again

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ function togglePasswordVisibility(textBoxID) {
         textBox.type = 'text';
     }
 }
+
+var slideNum = 0;
+slideShow();
+
+function slideShow() {
+    slideNum++;
+    console.log(slideNum);
+    var slides = document.getElementsByClassName("welcome-slide");
+    if(slideNum == slides.length) {
+        slideNum = 0;
+    }
+    for(var i = 0;i < slides.length;i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideNum].style.display = "flex";
+    setTimeout(slideShow,5000);
+}
+
