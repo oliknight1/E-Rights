29a987aa2053fab8169a723c6f431f05c04ee572
oliknight1 <oliknight2014@gmail.com>
2020-04-30 17:37:57 +0100

    Reformatted Navigation

--- account.php
+++ account.php
@@ -4,11 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/responsive-tablet.css">
+    <link rel="stylesheet" href="styles/responsive-mobile.css">
+    <script src="https://kit.fontawesome.com/96867cee00.js"></script>
     <title>E-Rights</title>
 </head>
 
 <body>
+    <nav>
+        <div class="logo">
+            <a href="home.php">
+                <img src=assets/logo/logo-horizontal-2.svg alt="Site Logo">
+            </a>
+        </div>
+        <div class="link-container">
+            <a href="#">
+                <i class="fas fa-home"></i>
+                <span>Home</span>
+            </a>
+            <a href="#">
+                <i class="fas fa-book-open"></i>
+                <p>All Courses</p>
+            </a>
+            <a href="#">
+                <i class="fas fa-graduation-cap"></i>
+                <span>My Learning</span>
+            </a>
+            <a href="#">
+                <i class="fas fa-user"></i>
+                <span>Account</span>
+            </a>
 
+
+        </div>
+    </nav>
 </body>
 
 </html>
