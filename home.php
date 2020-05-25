fa83284ae100bbdb9929901428f629ac54eb219c
oliknight1 <oliknight2014@gmail.com>
2020-05-25 20:21:01 +0100

    Navigation now all leads to the same place

--- home.php
+++ home.php
@@ -17,77 +17,119 @@ $user->findUser($_SESSION['user']);
     <link rel="stylesheet" href="styles/general-styles-mobile.css">
     <link rel="stylesheet" href="styles/general-styles-tablet.css">
     <script src="https://kit.fontawesome.com/96867cee00.js"></script>
-    <title>E-Rights</title>
-
 </head>
 
 <body>
-    <div class="home-wrapper">
-        <nav>
-            <div class="logo">
-                <a href="home.php">
-                    <img src=assets/logo/logo-horizontal-2.svg alt="Site Logo">
-                </a>
-            </div>
-            <div class="link-container">
-                <a href="#">
-                    <i class="fas fa-home"></i>
-                    <span>Home</span>
-                </a>
-                <a href="#">
-                    <i class="fas fa-book-open"></i>
-                    <p>All Courses</p>
-                </a>
-                <a href="#">
-                    <i class="fas fa-graduation-cap"></i>
-                    <span>My Learning</span>
-                </a>
-                <a href="#">
-                    <i class="fas fa-user"></i>
-                    <span>Account</span>
-                </a>
-            </div>
-        </nav>
+    <nav>
+        <!-- Hamburger Menu -->
 
-        <div class="home-info-container">
-            <div class="home-info-text-container">
+        <!-- Button that the user clicks -->
+        <i class="fas fa-bars" id="open-menu"></i>
 
-                <div class="home-info-text-title">
-                    <p>Hi <?php echo $user->getData()['username']; ?>!</p>
-                </div>
-                <div class="home-info-text-content">
-                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro in adipisci dolor quia obcaecati nemo
-                        quis! Tenetur magni deleniti illo quo sapiente laboriosam illum, qui minus totam hic ut quibusdam.</p>
-                </div>
-
-            </div>
-            <div class="home-info-profile-container">
-
-                <div class="profile-icon">
-                    <p><?php echo $user->getData()['username'][0]; ?></p>
-                </div>
-                <div class="home-info-profile-content">
-
-                    <div class="home-info-profile-content-box">
-
-                        <div class="home-info-profile-content-title">
-                            <p>Assigned</p>
+        <!-- Menu with links -->
+        <div class="hamburger-menu">
+            <header>
+                <p> Hi <?php echo $user->getData()['username'] ?>!</p>
+                <i class="fas fa-times" id="close-menu"></i>
+            </header>
+            <ul>
+                <li>
+                    <a href="home.php">
+                        <div class="icon-container">
+                            <i class="fas fa-home"></i>
                         </div>
-                        <div class="home-info-profile-content-amount">
-                            <p>4</p>
+                        <p>Home</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="courses.php">
+                        <div class="icon-container">
+                            <i class="fas fa-book-open"></i>
                         </div>
+                        <p>All Courses</p>
+                </li>
+                </a>
+                <li>
+                    <a href="my-learning.php">
+                        <div class="icon-container">
+                            <i class="fas fa-graduation-cap"></i>
+                        </div>
+                        <p> My Learning</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="account.php">
+                        <div class="icon-container">
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <p>Account</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
+        <!-- Navbar -->
+        <a href="home.php">
+            <img src=assets/logo/logo-horizontal-2.svg alt="Site Logo">
+        </a>
+
+        <div class="link-container">
+            <a href="home.php">
+                <i class="fas fa-home"></i>
+                <span>Home</span>
+            </a>
+            <a href="courses.php">
+                <i class="fas fa-book-open"></i>
+                <p>All Courses</p>
+            </a>
+            <a href="my-learning.php">
+                <i class="fas fa-graduation-cap"></i>
+                <span>My Learning</span>
+            </a>
+            <a href="account.php">
+                <i class="fas fa-user"></i>
+                <span>Account</span>
+            </a>
+        </div>
+
+    </nav>
+    <div class="home-info-container">
+        <div class="home-info-text-container">
+
+            <div class="home-info-text-title">
+                All Courses
+            </div>
+            <div class="home-info-text-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro in adipisci dolor quia obcaecati nemo
+                quis! Tenetur magni deleniti illo quo sapiente laboriosam illum, qui minus totam hic ut quibusdam.
+            </div>
+
+        </div>
+        <div class="home-info-profile-container">
+
+            <div class="profile-icon">
+                T
+            </div>
+            <div class="home-info-profile-content">
+
+                <div class="home-info-profile-content-box">
+
+                    <div class="home-info-profile-content-title">
+                        Assigned
+                    </div>
+                    <div class="home-info-profile-content-amount">
+                        4
                     </div>
 
-                    <div class="home-info-profile-content-box">
+                </div>
 
-                        <div class="home-info-profile-content-title">
-                            <p>Completed</p>
-                        </div>
-                        <div class="home-info-profile-content-amount">
-                            <p>0</p>
-                        </div>
+                <div class="home-info-profile-content-box">
 
+                    <div class="home-info-profile-content-title">
+                        Completed
+                    </div>
+                    <div class="home-info-profile-content-amount">
+                        0
                     </div>
 
                 </div>
@@ -96,15 +138,93 @@ $user->findUser($_SESSION['user']);
 
         </div>
 
+    </div>
+
+    <div class="home-wrapper">
+
+
+
+        <main class="all-courses-page">
+            <!-- Container around the list of courses -->
+            <div class="general-container">
+                <h2> Assigned</h2>
+                <!-- Course box -->
+                <div class="course-container">
+                    <img src="assets/illustrations/course-images/principles.svg" alt="Principles Course">
+                    <div class="course-info">
+                        <h3>Principles</h3>
+                        <!-- Empty div to group progress and label together for layout -->
+                        <div>
+                            <p id="progress-amount"> </p>
+                            <div class="progress-bar">
+                                <div class="progress-done" data-done="50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="course-container">
+                    <img src="assets/illustrations/course-images/principles.svg" alt="Principles Course">
+                    <div class="course-info">
+                        <h3>Principles</h3>
+                        <!-- Empty div to group progress and label together for layout -->
+                        <div>
+                            <p id="progress-amount"> </p>
+                            <div class="progress-bar">
+                                <div class="progress-done" data-done="50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="course-container">
+                    <img src="assets/illustrations/course-images/principles.svg" alt="Principles Course">
+                    <div class="course-info">
+                        <h3>Principles</h3>
+                        <!-- Empty div to group progress and label together for layout -->
+                        <div>
+                            <p id="progress-amount"> </p>
+                            <div class="progress-bar">
+                                <div class="progress-done" data-done="50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <!-- Container around the list of courses -->
+            <div class="general-container">
+                <h2> In Progress</h2>
+                <!-- Course box -->
+                <div class="course-container">
+                    <img src="assets/illustrations/course-images/principles.svg" alt="Principles Course">
+                    <div class="course-info">
+                        <h3>Principles</h3>
+                        <!-- Empty div to group progress and label together for layout -->
+                        <div>
+                            <p id="progress-amount"> </p>
+                            <div class="progress-bar">
+                                <div class="progress-done" data-done="50"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </main>
+
+
+    </div>
 
 
 
 
 
-
-
-        <div class="footer">
-            <div class="footer-row-container">
+    <footer>
+        <div class="footer-row-container">
+            <div class="footer-row-text">
                 <a href="#" class="footer-row-links">
                     Policy & Terms
                 </a>
@@ -114,28 +234,30 @@ $user->findUser($_SESSION['user']);
                 <a href="#" class="footer-row-links">
                     Help
                 </a>
-                <div class="footer-social-media">
-                    <a href="#" class="footer-social-media-icons">
-                        <i class="fab fa-facebook-square"></i>
-                    </a>
-                    <a href="#" class="footer-social-media-icons">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a href="#" class="footer-social-media-icons">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" class="footer-social-media-icons">
-                        <i class="fab fa-youtube"></i>
-                    </a>
-
-                </div>
-
             </div>
-            <div class="footer-image">
-                <img src=assets/logo/logo-horizontal-2.svg alt="404">
+            <div class="footer-social-media">
+                <a href="#" class="footer-social-media-icons">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="#" class="footer-social-media-icons">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#" class="footer-social-media-icons">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="footer-social-media-icons">
+                    <i class="fab fa-youtube"></i>
+                </a>
+
             </div>
 
         </div>
+        <div class="footer-image">
+            <img src=assets/logo/logo-horizontal-2.svg alt="404">
+        </div>
+
+    </footer>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
