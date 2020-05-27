6497cf294094821d542334ed735bd1d8389dcb45
SilasParker <silas.parker@gmail.com>
2020-05-27 14:50:06 +0100

    Reinserted styling and new JS for slider
    More HTML reinserted and adapted some Javascript
    Reinserted styling
    Doesn't work completely how it used to (slidein-right animation won't work)

--- js/main.js
+++ js/main.js
@@ -19,46 +19,67 @@ function togglePasswordVisibility(textBoxID) {
 
 //Onloads
 
-var slideNum = 0;
+var slideNum;
+var slides;
+var dots;
 
 window.onload = function() {
     if(document.querySelector(".welcome-body") !== null) {
-        this.slideShow();
+        slideNum = 0;
+        slides = document.getElementsByClassName("welcome-slide");
+        dots = document.getElementsByClassName("welcome-dot");
+        dots[0].onclick = function() { changeSlide(0); }
+        dots[1].onclick = function() { changeSlide(1); }
+        dots[2].onclick = function() { changeSlide(2); }
+        slideShow();
     }
     if(document.querySelector(".general-container")) {
-    fetchData();
-    document.getElementById("in-prog").onclick = function () { inProgressSelected(); }
-    document.getElementById("assigned").onclick = function () {
-            assignedSelected();
-        }
-    document.getElementById("completed").onclick = function () {
-            completedSelected();
-        }
+        fetchData();
+        document.getElementById("in-prog").onclick = function () { inProgressSelected(); }
+        document.getElementById("assigned").onclick = function () { assignedSelected(); }
+        document.getElementById("completed").onclick = function () {completedSelected();}
     }
 };
 
- function slideShow() {
-     slideNum++;
-     var slides = document.getElementsByClassName("welcome-slide");
-     if (slideNum == slides.length) {
-         slideNum = 0;
-     }
-     for (var i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     slides[slideNum].style.display = "flex";
+function slideShow() {
 
-     var dots = document.getElementsByClassName("welcome-dot");
-     for (var j = 0; j < dots.length; j++) {
-         dots[j].className = dots[j].className.replace(" welcome-dot-selected", "");
-     }
-     dots[slideNum].className += " welcome-dot-selected";
-     setTimeout(slideShow, 5000);
- }
+    slideNum++;
+    if(slideNum == slides.length) {
+        slideNum = 0;
+    }
+    for(var i = 0;i < slides.length;i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideNum].style.display = "flex";
 
+
+    for(var j = 0;j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" welcome-dot-selected","");
+    }
+    dots[slideNum].className += " welcome-dot-selected";
+    var viewCourseHref = document.getElementById("view-course").href;
+    var hiddenHrefText = slides[slideNum].getElementsByClassName("welcome-hidden-href")[0].textContent;
+    viewCourseHref = hiddenHrefText;
+    setTimeout(slideShow,5000);
+}
+
+function changeSlide(id) {
+    for(var i = 0;i < slides.length;i++) {
+        slides[i].style.display = "none";
+    }
+    slides[id].style.display = "flex";
+    for(var j = 0;j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" welcome-dot-selected","");
+    }
+    dots[id].className += " welcome-dot-selected";
+    var viewCourseHref = document.getElementById("view-course").href;
+    var hiddenHrefText = slides[id].getElementsByClassName("welcome-hidden-href")[0].textContent;
+    viewCourseHref = hiddenHrefText;
+    slideNum = id;
+}
 // Hamburger menu 
 
-const menuControl = e => {
+/*const menuControl = e => {
 
     const overlay = document.querySelector('.hamburger-menu');
     const overlayChildren = document.querySelector('.hamburger-menu>*');
@@ -73,13 +94,13 @@ const menuControl = e => {
     }
 }
 
-document.querySelector('#close-menu').addEventListener("click", menuControl);
-document.querySelector('#open-menu').addEventListener("click", menuControl);
+//document.querySelector('#close-menu').addEventListener("click", menuControl);
+//document.querySelector('#open-menu').addEventListener("click", menuControl);
 
 // Progress Bar Concept - will be adapted when the courses are created dynamically
 
 
-const progressBar = document.querySelector('.progress-done');
+//const progressBar = document.querySelector('.progress-done');
 // Use set timeout to add an animation to it
 setTimeout(() => {
     // Set the width of the blue section to how much progress has been made
@@ -87,4 +108,4 @@ setTimeout(() => {
     progressBar.style.opacity = 1;
     // Set text above progress bar to the amount of progress made
     document.querySelector('#progress-amount').textContent = 'Progress: ' + progressBar.getAttribute('data-done') + '%';
-}, 200)
+}, 200)*/
