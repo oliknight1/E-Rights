6639ff750b0e298d0a41a93f3102fc0e91a47830
TimDoulou <timchr2000@gmail.com>
2020-06-03 01:45:42 +0100

    fixed some welcome issues

--- js/main.js
+++ js/main.js
@@ -101,15 +101,7 @@ var currentInProg = 0;
 
 
 
-<<<<<<< HEAD
-var allAssigned = [];
-var allInProg = [];
-var currentAssigned = 0;
-var currentInProg = 0;
 
-window.onload = function() {
-    if(document.querySelector(".my-learning")) {
-=======
 window.onload = function () {
     if (document.querySelector(".my-learning")) {
         fetchData();
@@ -126,7 +118,6 @@ window.onload = function () {
         }
 
     } else if (document.querySelector(".home-wrapper") || document.querySelector(".all-courses-page")) {
->>>>>>> 6511d41951a242d32c535b9076efe67cb94b1d0e
         fetchData();
 
     } else if (document.querySelector(".welcome-body")) {
